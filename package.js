0da69522cc7d08a9154b3d8fdd693d1f210167fb
ochicf <oscarchicfarre@gmail.com>
2018-06-01 12:57:57 +0200

    update origenstudio:files-helpers dependency

--- package.js
+++ package.js
@@ -13,7 +13,7 @@ Package.onUse(api => {
     'ecmascript',
     'vulcan:core@1.10.1',
     'ostrio:files@1.9.11',
-    'origenstudio:files-helpers@0.0.1'
+    'origenstudio:files-helpers@0.0.2'
   ]);
 
   api.mainModule('lib/server/main.js', 'server');
